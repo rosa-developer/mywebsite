14b7aaff73eed4ebfd53dd5d526f1bd4fa33b994
rosa-developer <121453025+rosa-developer@users.noreply.github.com>
2025-08-02 00:07:25 -0400

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+# simple-interest.sh
+# This script calculates simple interest using the formula: (Principal * Rate * Time) / 100
+
+# Input values
+p=$1   # Principal
+r=$2   # Rate of Interest
+t=$3   # Time
+
+# Calculation
+si=$(( (p * r * t) / 100 ))
+
+# Output
+echo "Simple Interest: $si"
 
